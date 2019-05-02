02eb7709fd45032a25278305a5eb36a6fd2ed0f4
Erik Palm <erik.palm@ica.se>
2019-05-02 16:31:42 +0200

    Decode messages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const HttpsProxyAgent = require("https-proxy-agent")
 const url = require("url")
 const WebSocket = require("ws")
+const nmea = require("nmea-0183")
 
 const proxy = url.parse(process.env.PROXY_URL)
 const endpoint = `wss://api.oxyfi.com/trainpos/listen?v=1&key=${
@@ -10,4 +11,8 @@ const endpoint = `wss://api.oxyfi.com/trainpos/listen?v=1&key=${
 const agent = new HttpsProxyAgent(proxy)
 const socket = new WebSocket(endpoint, { agent })
 
-socket.on("message", data => console.log("data ->", data))
+function decodeGPGGASentence(sentence) {
+  return nmea.parse(sentence)
+}
+
+socket.on("message", data => console.log("data ->", decodeGPGGASentence(data)))

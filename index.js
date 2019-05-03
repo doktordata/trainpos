f5472bea825fdbe4ec97f23e9f883c4b1a876994
Erik Palm <erik.palm@ica.se>
2019-05-03 15:37:19 +0200

    Create server and client

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config()
+const express = require("express")
+const path = require("path")
 const HttpsProxyAgent = require("https-proxy-agent")
 const url = require("url")
 const WebSocket = require("ws")
+const { createServer } = require("http")
 const nmea = require("nmea-0183")
 
 const proxy = url.parse(process.env.PROXY_URL)
@@ -9,10 +12,27 @@ const endpoint = `wss://api.oxyfi.com/trainpos/listen?v=1&key=${
   process.env.API_KEY
 }`
 const agent = new HttpsProxyAgent(proxy)
-const socket = new WebSocket(endpoint, { agent })
+const trainPosSocket = new WebSocket(endpoint, { agent })
+const app = express()
+app.use(express.static(path.join(__dirname, "/public")))
+
+const server = createServer(app)
+const wss = new WebSocket.Server({ server })
 
 function decodeGPGGASentence(sentence) {
-  return nmea.parse(sentence)
+  const trainId = sentence.split(",").slice(-4)[0]
+  return {
+    ...nmea.parse(sentence),
+    trainId
+  }
 }
 
-socket.on("message", data => console.log("data ->", decodeGPGGASentence(data)))
+wss.on("connection", socket => {
+  const uniqueTrains = [] // Get unique train ids to render markers for
+  trainPosSocket.on("message", data => {
+    const decodedSentence = JSON.stringify(decodeGPGGASentence(data))
+    socket.send(decodedSentence)
+  })
+})
+
+server.listen(8080, () => console.log("Listening on http://localhost:8080"))
